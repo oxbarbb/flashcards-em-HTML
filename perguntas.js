a7978219492cbc88339021126b0bdbea0322f930
oxbarbb <joao.suski.matos@escola.pr.gov.br>
2025-10-10 08:03:51 -0300

    aula

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criacartao(
+    'programaçao',
+    'O que é python?',
+    'O python ´é uma linguagem de programação'
+)
+criacartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criacartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
